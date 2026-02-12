2f6d3393840ef3349c60cfad65698cd9b1c70cef
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-02-12 18:36:02 +0300

    Add a comment. Because I can

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,7 @@
 public class helloWorld{
     public static void main(String[] args){
+
+        // I wrote this comment because I can
         System.out.print("Hello world!\n");
         System.out.println("Wassup cuh!");
     }
