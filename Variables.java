884bbc6031e89622dbd1d94bbd64137f38359d45
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-02-13 06:50:36 +0300

    Add Scanner to accept user input and if statements to determine whether the student passed of failed

--- Variables.java
+++ Variables.java
@@ -1,3 +1,6 @@
+// import scanner for accepting user inpput
+import java.util.Scanner;
+
 public class Variables{
     public static void main(String[] args){
 
@@ -10,12 +13,37 @@ public class Variables{
         // varible declaration and initialization
 
         
-        int age;
-        age = 19;
-        String myName = "Benard Baraka";
-        double myGpa = 4.0;
+        int age ;
+        String userName;
+        String firstName;
+        String secondName;
+        double myGpa;
         int myYearOfStudy = 2;
         boolean isStudent;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name username: ");
+
+        userName = scanner.nextLine();
+
+        System.out.print("Hello " + userName + "\n");
+
+        System.out.print("Enter the following details: \n");
+
+        System.out.print("First Name: ");
+        firstName = scanner.nextLine();
+
+        System.out.print("Second Name: ");
+        secondName = scanner.nextLine();
+
+        System.out.print("AGE: ");
+        age = scanner.nextInt();
+
+        System.out.print("GPA: ");
+        myGpa = scanner.nextDouble();
+
+        scanner.close();
+
 
         if(myYearOfStudy <= 4 && myYearOfStudy >=1){
             isStudent = true;
@@ -27,10 +55,17 @@ public class Variables{
 
         
         
-        String myData = "Name: " + myName + "\nage: " + age + "\nGPA: " + myGpa;
+        String myData = "\nName: " + firstName + " " + secondName + "\nage: " + age + "\nGPA: " + myGpa;
 
         if(isStudent){
             System.out.println(myData);
+
+            if(myGpa >= 8){
+                System.out.println("You Passed!");
+            }
+            else{
+                System.out.println("You Failed!");
+            }
         }
         else{
             System.out.println("You are not a registered student.");
