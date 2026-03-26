b881219bf4e6f3b785d097ee31cdbb2d94794351
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-03-26 21:18:41 +0300

    GETTING ARRAY ELEMENTS FROM USER AFTER EXPLICITLY RECIEVING USER INPUT TO DETERMINE ARRAY SIZE

---
+++ arrays/arraysWithUserInput.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class arraysWithUserInput{
+    public static void main(String [] args){
+
+        String[] foods;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many foods would you like: ");
+        int size = scanner.nextInt();
+        scanner.nextLine();
+
+
+        foods = new String[size];
+
+        for(int i=0; i<foods.length; i++){
+            System.out.print("Enter a food: ");
+            foods [i] = scanner.nextLine();
+        }
+
+        for(String food : foods){
+            System.out.println(food);
+        }
+
+    }
+}
