e313eb4993eeed3bde8a0c5db516dc937f365a1a
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-03-29 05:42:20 +0300

    DECLARE A TWO DIMENSIONAL ARRAY AND ASSIGN ELEMENTS AND PRINT EACH ROW ON ITS OWN LINE IN THE CONSOLE

--- arrays/varArgs/twoDimensionalArrays.java
+++ arrays/varArgs/twoDimensionalArrays.java
@@ -5,17 +5,18 @@ class twoDimensionalArrays{
 
         // 2D Array - arrays where each element is an array
 
-        String [] fruits = {"mango", "orange", "apple"};
-        String [] veges = {"pizza", "hotdog", "hamburger"};
-        String [] meats = {"beef", "chicken", "pork"};
+        // 2D array declared once
+        String [][] groceries = {{"mangoes", "apples", "bananas"},
+                                 {"potato", "tomato", "carrots"},
+                                 {"chicken", "pork", "beef", "fish"}
+                                };
 
-        String [][] groceries ={fruits,veges,meats};
-
-
-        for(String[] foods: groceries){
+        for(String[] foods : groceries){
 
             for(String food : foods){
+
                 System.out.print(food + " ");
+
             }
             System.out.println();
         }
