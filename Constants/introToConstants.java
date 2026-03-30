30f30dc5add356d59ad429ca8ed501f79f1c58fc
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-03-30 13:40:35 +0300

    LEARN HOW TO DECLARE CONSTANT VARIABLES IN JAVA WITH NON-ACCESS MODIFIERS STATIC AND FINAL

---
+++ Constants/introToConstants.java
@@ -0,0 +1,14 @@
+class introToConstants{
+    static final double PI = 3.142;
+    public static void main(String [] args){
+
+        // java does not directly support constants
+        // to declare constatns in java we use the static and final non-access modifiers
+
+        // declare PI as a constant
+
+        
+
+        System.out.println(PI);
+    }
+}
