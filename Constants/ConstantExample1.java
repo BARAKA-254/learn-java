687faa29ebd965ca67e60478ed8aaabcfbbe8775
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-03-30 13:42:25 +0300

    USING CONSTANT VARIABLES IN JAVA TO CALCULATE TOTAL AFTER DECLARING PRICE AS CONSTANT

---
+++ Constants/ConstantExample1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+    public class ConstantExample1{
+    //declaring constant   
+    private static final double PRICE=234.90;
+    public static void main(String[] args){
+    int unit;
+    double total_bill;
+    System.out.print("Enter the number of units you have used:");
+    Scanner sc=new Scanner(System.in);
+    unit=sc.nextInt();
+    total_bill=PRICE*unit;
+    System.out.println("The total amount you have to deposit is: "+total_bill); 
+    }
+}
