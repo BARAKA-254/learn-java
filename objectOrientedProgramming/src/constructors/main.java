66351a17b9fcacc8de479e65a11b8a5d7774cb13
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-03 10:52:07 +0300

    MAKE ANOTHER STUDENT OBJECT USING THE EXITSTING CONSTUCTOR

--- objectOrientedProgramming/src/constructors/main.java
+++ objectOrientedProgramming/src/constructors/main.java
@@ -7,9 +7,25 @@ public class main {
 
         Student student1 = new Student("Spongebob",18,3.2);
         Student student2 = new Student("Patrick",21,1.5);
+        Student student3 = new Student("Sandy", 20, 4.2);
 
         System.out.println(student1.name);
+        System.out.println(student1.age);
+        System.out.println(student1.gpa);
+        System.out.println(student1.isEnrolled);
+        System.out.println();
+
         System.out.println(student2.name);
+        System.out.println(student2.age);
+        System.out.println(student2.gpa);
+        System.out.println(student2.isEnrolled);
+        System.out.println();
+        
+        System.out.println(student3.name);
+        System.out.println(student3.age);
+        System.out.println(student3.gpa);
+        System.out.println(student3.isEnrolled);
+        System.out.println();
     }
     
 }
