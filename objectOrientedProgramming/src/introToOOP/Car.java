46bf63e899983684aefc247721ec9413dad39670
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-03 07:01:05 +0300

    ADD ATTRIBUTES TO THE CAR CLASS

--- objectOrientedProgramming/src/introToOOP/Car.java
+++ objectOrientedProgramming/src/introToOOP/Car.java
@@ -5,4 +5,5 @@ public class Car{
     int year = 2021; 
     boolean isRunning = false;
     String color = "Red";
+    double price = 6000000;
 }
--- objectOrientedProgramming/src/introToOOP/main.java
+++ objectOrientedProgramming/src/introToOOP/main.java
@@ -1,9 +1,10 @@
 class main{
-    public static void main(String [] args){
+    public static void main(String[] args){
         
 
         Car car = new Car();
 
-        System.out.println("You drive a " + car.color + car.model);
+        System.out.println("You drive a " + car.color  + " " + car.model);
+        System.out.println("The " + car.make + " " + car.model + " costs Ksh " + car.price );
     }
 }
