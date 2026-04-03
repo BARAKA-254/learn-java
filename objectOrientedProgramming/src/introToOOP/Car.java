e6e123bee404b352df9acbed5bc9d4ba0e63f21d
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-03 06:46:15 +0300

    INTRODUCTION TO OOP

---
+++ objectOrientedProgramming/src/introToOOP/Car.java
@@ -0,0 +1,8 @@
+public class Car{
+
+    String make = "Ford";
+    String model = "Mustang";
+    int year = 2021; 
+    boolean isRunning = false;
+    String color = "Red";
+}
---
+++ objectOrientedProgramming/src/introToOOP/main.java
@@ -0,0 +1,9 @@
+class main{
+    public static void main(String [] args){
+        
+
+        Car car = new Car();
+
+        System.out.println("You drive a " + car.color + car.model);
+    }
+}
