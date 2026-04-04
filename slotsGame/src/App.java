4261c35439f817aae141dccfb61cdba9de239e34
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-04 17:44:47 +0300

    JAVA SLOT MACHINE

--- slotsGame/src/App.java
+++ slotsGame/src/App.java
@@ -1,5 +1,8 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+import java.util.Random;
+
+public class App{
+    public static void main(String [] args){
+        
     }
 }
