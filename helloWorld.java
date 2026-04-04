4f77c56c93ff142f796f4c3d586d4bdf38a1b5d2
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-04 07:25:10 +0300

    REMOVE UNECCESSARY FILES

--- helloWorld.java
+++
@@ -1,8 +0,0 @@
-public class helloWorld{
-    public static void main(String[] args){
-
-        // I wrote this comment because I can
-        System.out.print("Hello world!\n");
-        System.out.println("Wassup cuh!");
-    }
-}
