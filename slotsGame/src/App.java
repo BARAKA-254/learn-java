58da71bf0137cbbfcb41abdbd0f7f71882875b37
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-04 20:58:15 +0300

    ADD EMOJI STRINGS TO BE USED FOR THE GAME

--- slotsGame/src/App.java
+++ slotsGame/src/App.java
@@ -3,6 +3,30 @@ import java.util.Random;
 
 public class App{
     public static void main(String [] args){
+
+
+        // JAVA SLOT MACHINE
+        // DECLARE VARIABLES
+        int balance = 100;
+        int bet;
+        int payout;
+        String[] Row;
+
+        // WELCOME MESSAGE
+        System.out.println("********************************************");
+        System.out.println("WELCOME TO JAVA SLOT MACHINE. 🍒 🍉 🍋 ⭐ 🔔");
+        System.out.println("********************************************");
+        // DISPLAY BALANCE
         
+        // PLAY IF BALANCE IS > 0
+        // PLACE BET
+        //  CONFIRM IF BET IF GREATER THAN BALANCE
+        //  CONFIRM IF BET IS GREATER THAN 0
+        //  SUBTRACT BET FROM BALANCE
+        // SPIN ROW
+        // PRINT ROW
+        // GET PAYOUT
+        // ASK TO PLAY AGAIN
+        // DISPLAY EXIT MESSAGE
     }
 }
