e8f77e61ed7c97b4dbc26bbce682638e2663f158
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-07 20:16:36 +0300

    ADD A USER CONSTRUCTOR

--- objectOrientedProgramming/src/overloadedConstructors/Main.java
+++ objectOrientedProgramming/src/overloadedConstructors/Main.java
@@ -1,5 +1,9 @@
 public class Main{
     public static void main(String[] args){
+
+        // Overloaded Constructors - allow a class to have multiple constructors with different parameter lists
+        //                         - allows objects to be initialized in various ways
         
+        User user1 = new User("Spongebob");
     }
 }
---
+++ objectOrientedProgramming/src/overloadedConstructors/User.java
@@ -0,0 +1,9 @@
+public class User {
+    String username;
+    String email;
+    int age;
+
+    User(String username){
+        this.username = username;
+    }    
+}
