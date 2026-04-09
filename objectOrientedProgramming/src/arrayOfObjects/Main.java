13ceacd94efca9f3eb61afa8c051148b14283502
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-09 21:05:38 +0300

    CREATE CAR OBJECTS

--- objectOrientedProgramming/src/arrayOfObjects/Main.java
+++ objectOrientedProgramming/src/arrayOfObjects/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
         
+        Car car1 = new Car("BMW", "blue");
+        Car car2 = new Car("MERCEDES", "grey");
+        Car car3 = new Car("AUDI", "black");
     }
 }
