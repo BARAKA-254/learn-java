3cba6d4a136158b851874bdcc8cc5b7681c60adb
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-11 19:59:13 +0300

    CREATE DOG AND CAT CLASSES THAT INHERIT FROM THE ALIVE FIELD FROM THE ANIMAL CLASS

---
+++ objectOrientedProgramming/src/Inheritance/Animal.java
@@ -0,0 +1,7 @@
+public class Animal{
+    boolean isAlive;
+
+    Animal(){
+        isAlive = true;
+    }
+}
---
+++ objectOrientedProgramming/src/Inheritance/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // INHERITANCE  =   One class inherits the methods and attributes from another class
+        //                  Child inherits from parent
+
+        Cat myCat = new Cat();
+
+        System.out.println(myCat.isAlive);
+    }
+    
+}
