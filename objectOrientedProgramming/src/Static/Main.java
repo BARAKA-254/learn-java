864706158eff33aee10ad8625e54729d34eb716f
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-11 13:28:56 +0300

    ACCESS THE STATIC VARIABLE (numOfFriends) USING THE CLASS [FRIENDS] NAME INSTEAD OF THE OBJECT NAMES

--- objectOrientedProgramming/src/Static/Main.java
+++ objectOrientedProgramming/src/Static/Main.java
@@ -8,15 +8,16 @@ public class Main{
         Friend friend2 = new Friend("Patrick");
         Friend friend3 = new Friend("Sandy");
 
+        // accessing the static variable (numOfFriends) from the class name instead of the object name [not neccessary - best practice]
         System.out.println(friend1.name);
-        System.out.println(friend1.numOfFriends);
+        System.out.println(Friend.numOfFriends);
         System.out.println();
 
         System.out.println(friend2.name);
-        System.out.println(friend2.numOfFriends);
+        System.out.println(Friend.numOfFriends);
         System.out.println();
         
         System.out.println(friend3.name);
-        System.out.println(friend3.numOfFriends);
+        System.out.println(Friend.numOfFriends);
     }
 }
