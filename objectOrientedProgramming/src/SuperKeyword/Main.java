d41e001a4fc741becf2d52ddfab5944a590b5011
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-12 21:01:21 +0300

    LEARN ABOUT THE SUPER KEYWORD IN JAVA

---
+++ objectOrientedProgramming/src/SuperKeyword/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        //Super =   refers to the parent class
+        //          used in method overriding
+        //          calls the parent constructor to initialize attributes
+    }
+}
