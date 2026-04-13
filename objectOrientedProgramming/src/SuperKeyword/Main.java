b93d56cd762a8757a0f41010057f25133f277421
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-14 00:20:43 +0300

    CREATEA A PERSON OBJECT AND CALLL THE SHOWNAME METHOD

--- objectOrientedProgramming/src/SuperKeyword/Main.java
+++ objectOrientedProgramming/src/SuperKeyword/Main.java
@@ -1,7 +1,12 @@
+
 public class Main{
     public static void main(String[] args){
         //Super =   refers to the parent class
         //          used in method overriding
         //          calls the parent constructor to initialize attributes
+
+        Person person1 = new Person("Benard", "Baraka");
+
+        person1.showName();
     }
 }
