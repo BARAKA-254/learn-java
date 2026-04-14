a3d5eba8f52e976f64b4e7118b55ceba2cbcb804
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-14 13:32:24 +0300

    CREATE A CAR CLASS AND ITS OBJECT, PASS THE DETAILS AND PRINT THEM OUT

---
+++ objectOrientedProgramming/src/toString/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+ 
+        // toString() = Method inherited from the object class
+        //              Used to return a string representation of an object
+        //              By default it returns a hash code as a unique identifier
+        //              It can be overriden to provide meaningful details
+
+
+        Car car = new Car("BMW","M5CS", 2026, "BLUE");
+
+        System.out.println(car.year + " " + car.color + " " + car.make + " " + car.model);
+    }
+}
