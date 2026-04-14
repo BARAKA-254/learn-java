ad4d59eeea06df591e52346aa2b815286714ad43
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-14 12:03:11 +0300

    LEARN METHOD OVERRIDING

---
+++ objectOrientedProgramming/src/methodOverriding/Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[] args){
+        
+        // Method Overriding = When a subclass provides a method implementation of a class that is aready defined
+        //                   = Allows for code reusability and give specific implementation
+
+        
+    }
+}
