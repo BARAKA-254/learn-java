b3ca2dcd6828c4bfd69358a0f52176d31620f219
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-15 12:55:15 +0300

    IMPLEMENT THE AREA METHOD IN EVERY SHAPE

--- objectOrientedProgramming/src/Abstration/Circle.java
+++ objectOrientedProgramming/src/Abstration/Circle.java
@@ -1,7 +1,13 @@
 class Circle extends Shape{
 
+    double radius;
+
+    Circle(double radius){
+        this.radius = radius;
+    }
+
    @Override
    double area(){
-    return 0;
+    return Math.PI * radius * radius;
    }
 }
