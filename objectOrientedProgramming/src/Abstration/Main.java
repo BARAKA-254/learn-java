4411057d0f87c91a6683ed84e0bcba270c6b6560
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-15 12:40:49 +0300

    LEARN ABSTRACTION

---
+++ objectOrientedProgramming/src/Abstration/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        
+        //  Abstraction = the process of hiding the implementation details and showing only essential features
+        //  abstract    = used to define abstract classes and methods
+        //                Abstract classes can't be instatiated directly
+        //                Can contain abstract methods which means they must be implemented
+        //                Can contain concrete methods which means they are inherited
+
+        
+    }
+}
