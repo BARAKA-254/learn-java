67dc80bcbbd61520adef03f322bff8840f9c9f4d
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-15 13:09:23 +0300

    add a comment

--- objectOrientedProgramming/src/Abstration/Shape.java
+++ objectOrientedProgramming/src/Abstration/Shape.java
@@ -2,7 +2,7 @@ public abstract class Shape{
     
     abstract double area(); //ABSTRACT METHOD
 
-    void displayInfo(){
+    void displayInfo(){     //CONCRETE METHOD
         System.out.println("This is a Shape");
     }
 
