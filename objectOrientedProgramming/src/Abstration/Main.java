b5c6e8b9f704562985aae5dc359da26ad77636c8
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-15 13:03:52 +0300

    CREATE MULTIPLE SHAPE OBJECT AND CALL THE AREA METHOD

--- objectOrientedProgramming/src/Abstration/Main.java
+++ objectOrientedProgramming/src/Abstration/Main.java
@@ -7,6 +7,12 @@ public class Main{
         //                Can contain abstract methods which means they must be implemented
         //                Can contain concrete methods which means they are inherited
 
-        
+        Circle circle = new Circle(7);
+        Triangle triangle = new Triangle(3, 4);
+        Rectangle rectangle = new Rectangle(5, 6);
+
+        System.out.println("The area of a rectangle "+ rectangle.area());
+        System.out.println("The area of the circle is " +circle.area());
+        System.out.println("The area of the triangle is " + triangle.area());
     }
 }
