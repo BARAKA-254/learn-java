af2f9d06e6142f9c2912693d29bfe60e9479073d
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-17 21:41:54 +0300

    LEARN POLYMORPHISM

---
+++ objectOrientedProgramming/src/Polymorphism/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        // Polymorphism =   an oop concept that describes that an object can take different forms
+        // Poly         =   means many
+        // morph        =   means shape
+        //                  objects can identify as other objects
+        //                  objects can be treated as objects of a common superclass
+    }
+}
