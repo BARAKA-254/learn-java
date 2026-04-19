96adb1efe18de6b17a4e838f909eb6a0bde916a1
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-19 11:55:38 +0300

    ADD COMMENTS TO EXPLAIN THE TOPIC

--- objectOrientedProgramming/src/getterSetter/Main.java
+++ objectOrientedProgramming/src/getterSetter/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args){
 
+        // They help protect data and add rules for accesing and modifiying data
+        // GETTERS  =   methods that make a field READABLE
+        // SETTERS  =   methods that make a field WRITEABLE
 
         Car car = new Car("BMW", "M5", 2025, "BLUE", 10_000_000);
 
