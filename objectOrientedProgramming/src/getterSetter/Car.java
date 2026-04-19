5a2a2e4e35a594d224a1ff818c82afcd65d5773a
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-19 11:04:40 +0300

    LEARN ABOUT GETTER AND SETTER METHODS FOR ACCESSING PRIVATE DATA FROM OTHER CLASSES

---
+++ objectOrientedProgramming/src/getterSetter/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+    private String color;
+    private int price;
+
+    Car(String make, String model, int year, String color, int price){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+    }
+
+}
---
+++ objectOrientedProgramming/src/getterSetter/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+
+
+        Car car = new Car("BMW", "M5", 2025, "BLUE", 10_000_000);
+
+      
+    }
+}
