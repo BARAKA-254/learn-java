34facfdc1122dd1192ac7311012e17f1d45008ee
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-21 05:33:34 +0300

    ADD A displayInfo() METHOD IN THE FACULTY CLASS TO DISPLAY ALL THE COURSES OFFERED IN THE FACULTY

--- objectOrientedProgramming/src/Aggregation/AggregationExample2/Faculty.java
+++ objectOrientedProgramming/src/Aggregation/AggregationExample2/Faculty.java
@@ -3,12 +3,23 @@ public class Faculty{
     String course1;
     String course2;
     String course3;
+    String course4;
 
 
-    Faculty(String name, String course1, String course2, String course3){
+    Faculty(String name, String course1, String course2, String course3, String course4){
         this.name = name;
         this.course1 = course1;
         this.course2 = course2;
         this.course3 = course3;
+        this.course4 = course4;
+    }
+
+    public void displayInfo(){
+        System.out.println("Faculty name: " + this.name);
+        System.out.println("COURSES OFFERED: ");
+        System.out.println(this.course1);
+        System.out.println(this.course2);
+        System.out.println(this.course3);
+        System.out.println(this.course4);
     }
 }
--- objectOrientedProgramming/src/Aggregation/AggregationExample2/Main.java
+++ objectOrientedProgramming/src/Aggregation/AggregationExample2/Main.java
@@ -1,17 +1,16 @@
-import javax.xml.stream.FactoryConfigurationError;
-
 public class Main{
     public static void main(String[] args){
         
-        Faculty SPAS = new Faculty("School of Pure and Applied Sciences", "B CS", "B SWE", "B IT");
-        Faculty ENG = new Faculty("School Of Engineering", "Mechanical Engineering", "Electrical Engineering", "Civil Engineering");
+        Faculty SPAS = new Faculty("School of Pure and Applied Sciences", "BSC CS", "BSC SWE", "BSC IT", "BSC PHYSICS");
+        Faculty ENG = new Faculty("School Of Engineering", "Mechanical Engineering", "Electrical Engineering", "Civil Engineering", "Mechatronics Engineering");
+        Faculty HEALTH = new Faculty("School Of Medicine and Health Sciences", "Medicine", "Nursing", "Pharmacy", "Medical Engineering");
 
-        Faculty[] faculties = {SPAS,ENG};
+        Faculty[] faculties = {SPAS,ENG,HEALTH};
         
         University KYU = new University("Kirinyaga University", 1973, faculties);
 
         System.out.println(KYU.name);
-        System.out.println(SPAS.name);
+        SPAS.displayInfo();
 
     }
 }
