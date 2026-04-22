09d3f05446c2a24a2e3c7cf42533139b3e5959b2
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-22 20:26:37 +0300

    LEARN COMPOSITION

---
+++ objectOrientedProgramming/src/Composition/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void mai(String[] args){
+        //Composition   =   Represents a "part of" relationship between objects.
+        //                  For example an engine is part of a car
+        //                  Allows complex object to be constructed from smaller object
+    }
+}
