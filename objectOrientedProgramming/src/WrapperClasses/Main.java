cd9d89449962110f9a4007f75df86e7c5f732799
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-24 09:11:33 +0300

    USING AUTOBOXING AND UNBOXINGN TO CONVERT PRIMITIVE TYPE VARIABLES TO OBJECTSJ AND VICE VERSA

---
+++ objectOrientedProgramming/src/WrapperClasses/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args){
+    
+        // Wrapper classes  =   Allow primitive values (int, char, double, boolean) to be used as objects. "Wrap them in an object"
+        //                      Generally, don't wrap primitives unless you want an object
+        //                      Allows use of collections Framework and static Utility Methods
+
+
+        /*
+        // The method of wrapping below is depreciated and only used for demonstration
+
+        Integer a = new Integer(123);
+        Double b = new Double(3.14);
+        Character c = new Character("&");
+        Boolean d = new Boolean(true);
+        */
+
+        // A modern approach to create wrapper classes is the one below(known as autoboxing)
+        
+        // Autoboxing
+        Integer a = 123;
+        Double b = 3.14;
+        Character c = '$';
+        Boolean d = true;
+        // similar to the creation of strings eg
+        String e = "Elephant";
+
+        //  Unboxing = converting a wrapper class back to it's primitive
+        // to unbox , assign the wrapper class object to a variable declared with it's primitive datatype
+        int x = a;
+        double y = b;
+        char z = c;
+        boolean g = d;
+
+    }   
+}
