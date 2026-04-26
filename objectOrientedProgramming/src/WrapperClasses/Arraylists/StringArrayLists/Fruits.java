bc1c453a0bc0c18caab508f17eda4ad323c49901
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-26 15:02:05 +0300

    TRY OUT DIFFERENT ARRAYLIST METHDOS

---
+++ objectOrientedProgramming/src/WrapperClasses/Arraylists/StringArrayLists/Fruits.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Fruits {
+    public static void main(String[] args) {
+        
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Orange");
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Lemon");
+         
+        System.out.println(fruits);
+
+        // get method to return the element at the index passed into the method
+        String first = fruits.get(0);
+        System.out.println(first);
+
+        // remove method to remove the element at the specified index
+        fruits.remove(0);
+        System.out.println(fruits);
+        fruits.add("Orange");
+
+        // set method replaces the item in the specified index with the other one passed withing the method
+        fruits.set(0, "Pineapple");
+        System.out.println(fruits);
+
+    }
+}
