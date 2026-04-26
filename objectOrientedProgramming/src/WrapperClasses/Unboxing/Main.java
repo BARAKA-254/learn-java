8e38e771eea626fde6402a792ea2be361bd15378
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-26 07:21:30 +0300

    LEARN ABOUT UNBOXING

---
+++ objectOrientedProgramming/src/WrapperClasses/Unboxing/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args){
+
+
+        //Unboxing  =   is the process of converting wrapper class objects back to their respective primitives
+
+        Integer a = 123;
+        Double b = 3.14;
+        Character c = '@';
+        Boolean d = true;
+
+        // assign the object variable to a variable declared as primitive datatype
+        int x = a;
+        double y = b;
+        char z = c;
+        boolean w = d;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(w);
+    }
+}
