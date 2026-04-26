96504429817f7716b6b744d6492c06b7a3b6588e
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-26 06:55:07 +0300

    LEARN ABOUT AUTOBOXING IN JAVA

---
+++ objectOrientedProgramming/src/WrapperClasses/Autoboxing/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        //Autoboxing =  is converting primitive data types to classes
+        //              used incase there is a need for objects eg when dealing with arraylists
+
+        //convert an int primitive data type to a class
+        Integer a = 123;
+
+        //create a class of a primitive data type double
+        Double b = 3.14;
+
+
+        //Character
+        Character c = '@';
+
+        //boolean
+        Boolean d = true;
+
+        //By wrapping these primitive datatypes in objects. We can use them in collections frameworks such as arraylists
+    }
+}
