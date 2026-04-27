fd70be9e6680d7fdfb4af9dfb0daafac91f5d1bb
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-27 16:59:37 +0300

    LEARN ABOUT FILE WRITTING

---
+++ objectOrientedProgramming/src/fileHandling/fileWriting/Intro.java
@@ -0,0 +1,12 @@
+public class Intro {
+    public static void main(String[] args) {
+        // There are 4 popular options for writing files in java
+        
+        // 1. FileWriter        = Good for small or medium-sized text files
+        // 2. BufferedWriter    = Better perfomance for large amounts for text
+        // 3. PrintWriter       = Best for structured data, like reports or logs
+        // 4. FileOutputStream  = Best for binary files (e.g. images, audio files)
+        
+        
+    }
+}
