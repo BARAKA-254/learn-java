8dbd93412dd75583ddfb3d401124139544e2b11f
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-27 19:12:37 +0300

    ADD FUNCTIONALITY TO TYPE MULTIPLE LINES UPTO 5

--- objectOrientedProgramming/src/fileHandling/fileWriting/FileWriting/FileWriter/fileWriting/src/Main.java
+++ objectOrientedProgramming/src/fileHandling/fileWriting/FileWriting/FileWriter/fileWriting/src/Main.java
@@ -1,12 +1,30 @@
 import java.io.FileWriter;
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         // Using FileWriter fo create files
 
-        try (FileWriter writer = new FileWriter("test.txt")) {
-            System.out.println("File has been written succesfully");
+        try (FileWriter writer = new FileWriter("test.txt"); Scanner scanner = new Scanner(System.in)) {
 
-            writer.write("wazuuup twin");
+            int fileCounter = 0;
+            String userFile = " ";
+
+            System.out.println("Type you file here: (type : [fileWriter._save] ) to save file: ");
+            while(fileCounter <= 5){
+                
+                if(fileCounter == 0){
+                    userFile = scanner.nextLine().toUpperCase();
+                }
+                else{
+                    userFile = userFile + "\n" + scanner.nextLine();
+                }
+
+                fileCounter++;
+            }
+            
+            System.out.println("File has been saved");
+
+            writer.write(userFile);
         } 
         catch (Exception e) {
             System.out.println("Something went wrong");    
