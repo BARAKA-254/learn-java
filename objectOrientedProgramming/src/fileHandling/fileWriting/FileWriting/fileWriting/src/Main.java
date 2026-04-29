a323a4058dd80d7242bdf0e7c03d3f288df32a73
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-29 15:47:17 +0300

    ADD A SCANNER TO LET THE USER SPECIFY FILENAME AND EXTENSION

--- objectOrientedProgramming/src/fileHandling/fileWriting/FileWriting/fileWriting/src/Main.java
+++ objectOrientedProgramming/src/fileHandling/fileWriting/FileWriting/fileWriting/src/Main.java
@@ -1,15 +1,26 @@
 import java.io.FileWriter;
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         // Using FileWriter fo create files
 
-        try (FileWriter writer = new FileWriter("test.txt")) {
-          System.out.println("File written successfully!!");  
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your fileName along with the extesion: ");
+        String fileName = scanner.nextLine();
+
+        try (FileWriter writer = new FileWriter(fileName)) {
+          System.out.println("File written successfully!!");
+          
+          writer.write("Wassup twin");
         } 
         
-        catch (Ex e) {
+        catch (Exception e) {
             // TODO: handle exception
             System.out.println("Something went wrong");
+        }
+
+        finally{
+            scanner.close();
         }
         
     }
