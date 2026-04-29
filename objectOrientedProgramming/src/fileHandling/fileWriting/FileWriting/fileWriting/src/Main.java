a420adec9793e393df4845d35f0b8867a830be73
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-29 15:55:04 +0300

    ADD A LINECOUNTER AND WHILE LOOP TO LIMIT THE NUMBER OF LINE A USER CAN WRITE

--- objectOrientedProgramming/src/fileHandling/fileWriting/FileWriting/fileWriting/src/Main.java
+++ objectOrientedProgramming/src/fileHandling/fileWriting/FileWriting/fileWriting/src/Main.java
@@ -1,27 +1,38 @@
 import java.io.FileWriter;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 public class Main{
     public static void main(String[] args){
         // Using FileWriter fo create files
+
+        String textLine = " ";
+        int lineCounter = 0;
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your fileName along with the extesion: ");
         String fileName = scanner.nextLine();
 
-        try (FileWriter writer = new FileWriter(fileName)) {
-          System.out.println("File written successfully!!");
+        while (lineCounter < 5) {
+            try (FileWriter writer = new FileWriter(fileName)) {
+                System.out.println("File written successfully!!");
           
-          writer.write("Wassup twin");
-        } 
-        
-        catch (Exception e) {
-            // TODO: handle exception
-            System.out.println("Something went wrong");
-        }
+                writer.write("Wassup twin");
 
-        finally{
-            scanner.close();
+                lineCounter++;
+            } 
+            catch(FileNotFoundException e){
+                System.out.println("File couldn't be printed!!");
+            }
+            catch (Exception e) {
+                // TODO: handle exception
+                System.out.println("Something went wrong");
+            }
+
+            finally{
+                scanner.close();
+            }
         }
+       
         
     }
 }
