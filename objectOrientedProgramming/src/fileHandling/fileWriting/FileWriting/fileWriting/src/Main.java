3f7293f7b7fd71066425dda7531c1e7fe4eb6cc7
baraka <201593787+BARAKA-254@users.noreply.github.com>
2026-04-30 14:30:32 +0300

    iMPLEMENT A WHILE LOOP TO ACCEPT MULTIPLE LINES AS USER INPUT

--- objectOrientedProgramming/src/fileHandling/fileWriting/FileWriting/fileWriting/src/Main.java
+++ objectOrientedProgramming/src/fileHandling/fileWriting/FileWriting/fileWriting/src/Main.java
@@ -12,31 +12,37 @@ public class Main{
         System.out.print("Enter your fileName along with the extesion: ");
         String fileName = scanner.nextLine();
 
-        //while (lineCounter < 5) {
-            try (FileWriter writer = new FileWriter(fileName)) {
+        try (FileWriter writer = new FileWriter(fileName)) {
                 
-                System.out.println("WRITE YOUR FILE HERE: ");
-                textLine = scanner.nextLine();
+            System.out.println("WRITE YOUR FILE HERE: ");
+                while (lineCounter < 5) {
+                    if(lineCounter == 0){
+                    textLine = scanner.nextLine();
+                }
+                else{
+                    textLine = textLine + " \n" + scanner.nextLine();
+                }
 
+                System.out.println("File writtenn succesfully");
                 lineCounter++;
-
-                writer.write(textLine);
-            } 
-            catch(FileNotFoundException e){
-                System.out.println("File couldn't be printed!!");
-            }
-            catch (Exception e) {
-                // TODO: handle exception
-                System.out.println("Something went wrong");
-            }
-
-            finally{
-                scanner.close();
-            }
+            }   
             
-        //}
-        
-       
+            
+
+            writer.write(textLine);
+        } 
+                
+        catch(FileNotFoundException e){
+            System.out.println("File couldn't be printed!!");
+        }
+        catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Something went wrong");
+        }
+
+        finally{
+            scanner.close();
+        } 
         
     }
 }
